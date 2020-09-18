f35bd90623a074fe27b939b5495ec53bfed58025
Lekkala <878127@cognizant.com>
2020-09-18 12:16:56 +0800

    Change Word count streaming program to point to local kafka setup

--- src/main/scala/com/example/kafka/streams/Config.scala
+++ src/main/scala/com/example/kafka/streams/Config.scala
@@ -10,23 +10,10 @@ object Config {
   def getConfig: Properties = {
     val props = new Properties
     props.put(StreamsConfig.APPLICATION_ID_CONFIG,"word-count-stream-app")
-    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "tricycle-01.srvs.cloudkafka.com:9094,tricycle-02.srvs.cloudkafka.com:9094,tricycle-03.srvs.cloudkafka.com:9094")
+    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
     props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass.getName)
     props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String.getClass.getName)
-    props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, "10000")
-    props.put("auto.offset.reset", "earliest")
-    props.put("enable.auto.commit", "true")
-    props.put("auto.commit.interval.ms", "1000")
-    props.put("session.timeout.ms", "30000")
-    props.put("security.protocol", "SASL_SSL")
-    props.put("sasl.mechanism", "SCRAM-SHA-256")
-    props.put("sasl.jaas.config", getJaasCFG)
     props
   }
 
-  def getJaasCFG : String = {
-    val username = "s78m90xp"
-    val password = "JsAkTX90KYhpkceLUY32PxnLtfcjURIk"
-    s"org.apache.kafka.common.security.scram.ScramLoginModule required username=$username password=$password;"
-  }
 }
--- src/main/scala/com/example/kafka/streams/WordCountStreamingApplication.scala
+++ src/main/scala/com/example/kafka/streams/WordCountStreamingApplication.scala
@@ -1,25 +1,27 @@
 package com.example.kafka.streams
 
-import java.lang
-
-import org.apache.kafka.common.serialization.{Serde, Serdes}
+import org.apache.kafka.common.serialization.Serde
 import org.apache.kafka.streams.KafkaStreams
-import org.apache.kafka.streams.kstream.Materialized
-import org.apache.kafka.streams.scala.StreamsBuilder
+import org.apache.kafka.streams.kstream.{Materialized, Produced}
+import org.apache.kafka.streams.scala.{Serdes, StreamsBuilder}
 import org.apache.kafka.streams.scala.kstream.{Consumed, Grouped}
 
 object WordCountStreamingApplication extends App {
 
-  val inputTopic = "s78m90xp-word-count-input"
-  val outputTopic = "s78m90xp-word-count-output"
+  val inputTopic = "word-count-input"
+  val outputTopic = "word-count-output"
 
-  private val stringSerde: Serde[String] = Serdes.String()
+  private val stringSerde: Serde[String] = Serdes.String
+  private val longSerde : Serde[Long] = Serdes.Long
   private val builder = new StreamsBuilder()
   implicit private val consumed: Consumed[String, String] = Consumed.`with`(stringSerde, stringSerde)
+
   builder.stream(inputTopic)
     .flatMapValues(line => line.toLowerCase.split(" "))
     .groupBy((_, word) => word)(Grouped.`with`(stringSerde, stringSerde))
-    .count()(Materialized.as(outputTopic))
+    .count()(Materialized.`with`(stringSerde, longSerde))
+    .toStream
+    .to(outputTopic)(Produced.`with`(stringSerde, longSerde))
 
 
   private val streams = new KafkaStreams(builder.build(), Config.getConfig)
